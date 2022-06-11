8cd6c6d9873d350e17509637b6dde64d00969700
Furkan Yenidoğan <72672265+furkanyenidogan@users.noreply.github.com>
2022-06-12 01:08:22 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /**
+     *
+     */
+    private static final String HELLO_WORLD = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println(HELLO_WORLD);
+    }
+    
+}
