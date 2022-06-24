c1dba80cadbeeeba9f0e96acecfb1e675d15bcb1
Furkan Yenidoğan <72672265+furkanyenidogan@users.noreply.github.com>
2022-06-25 00:51:04 +0300

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,25 @@
+public class Factorial {
+    public static void main(String[] args) {
+       int a = Factorial.combination(5, 2);
+       
+       System.out.println(a);
+        
+    }
+    
+    public static int factorial(int number) {   
+            if (number == 0)    
+              return 1;    
+            else    
+              return(number * factorial(number-1));    
+           }   
+           
+    public static int combination( int n , int r){
+        int result;
+
+        result = factorial(n) / (factorial(r) * factorial(n-r));
+        
+        return result;
+
+    }
+        
+}
